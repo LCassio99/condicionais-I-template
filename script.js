b3c497a34c7499eef5a68acde0a2c4e4b8ee944e
LCassio99 <lucianocassio66@gmail.com>
2023-05-19 20:29:05 -0300

    práticas de aula sobre condicionai 1

---
+++ script.js
@@ -0,0 +1,79 @@
+const booleano1 = true
+const booleano2 = false
+const booleano3 = true
+
+// if(booleano1){
+
+//   alert('Ulalau')
+
+// }else{
+
+//   alert('Só Chora')
+
+// }
+
+//----------------------------------------------------
+
+// if(booleano1 === booleano2){
+
+//   alert('1 = 2')
+
+// } else if(booleano2 === booleano3){
+
+//   alert('2 = 3')
+
+// } else if(booleano1 === booleano3){
+
+//   alert('1 = 3')
+
+// } else{
+
+//   alert('Nada com nada')
+
+// }
+
+//-------------------------------------------------------
+
+// let idade = prompt('Qual sua idade?')
+
+// if(idade >= 18 && idade < 70){
+
+//   alert('Pode e deve votar!')
+
+// }else if(idade < 16){
+
+//   alert('Não pode votar bb!')
+
+// }else if(idade >= 16 || idade <= 17 || idade >= 70){
+
+//   alert('Tu que sabe!')
+
+// }else{
+
+//   alert('Tu existe?')
+
+// }
+
+//-------------------------------------------------------
+
+let media = prompt('Bem-vindo(a) ao sistema do estudante! Digite sua média abaixo para ver sua situação:')
+
+if(media >= 5 && media < 6){
+
+  alert('Você foi APROVADO(A)! XD')
+
+}else if(media >= 3 && media < 5){
+
+  alert('Mais uam chance de ser feliz... u.u')
+
+}else if(media < 3){
+
+  alert('Foi triste...')
+
+}else{
+
+  alert('Dado inválido...')
+
+}
+
+
